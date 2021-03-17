4f596a95661082b9bb5b52c8eb85cf12a065bb2d
Terrance Fisher <teefisher314@gmail.com>
2021-03-17 16:11:47 -0500

    finished part 3

--- queries.sql
+++ queries.sql
@@ -1,11 +1,13 @@
 ## Part 1: Test it with SQL
-private int id;
-private int employer;
-private String name;
-private String skills;
+id INT;
+employer VARCHAR(50);
+name VARCHAR(50);
+skills VARCHAR(100);
 
 ## Part 2: Test it with SQL
-
+SELECT *
+FROM techjobs.employer
+WHERE location= ("St. Louis City");
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
